72aaa4ac246bb556758c2087f3f3ba31924139e1
cotton-moon <wooyueting@gmail.com>
2025-07-16 17:43:03 +0100

    Update and rename 5blockiat7.js to 5blockiat8.js

--- 5blockiat8.js
+++ 5blockiat8.js
@@ -116,16 +116,16 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			
 			////In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
 			////If you set the number of trials in any block to 0, that block will be skipped.
-			blockAttributes_nTrials : 20,
-			blockAttributes_nMiniBlocks : 5,
-			blockCategories_nTrials : 20,
-			blockCategories_nMiniBlocks : 5,
-			blockFirstCombined_nTrials : 60,
-			blockFirstCombined_nMiniBlocks : 5,
+			blockAttributes_nTrials : 12,
+			blockAttributes_nMiniBlocks : 12,
+			blockCategories_nTrials : 12,
+			blockCategories_nMiniBlocks : 12,
+			blockFirstCombined_nTrials : 24,
+			blockFirstCombined_nMiniBlocks : 24,
 			blockSecondCombined_nTrials : 0, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
 			blockSecondCombined_nMiniBlocks : 10, 
-			blockSwitch_nTrials : 28,
-			blockSwitch_nMiniBlocks : 7,
+			blockSwitch_nTrials : 24,
+			blockSwitch_nMiniBlocks : 24,
 
 			//Should we randomize which attribute is on the right, and which on the left?
 			randomAttSide : false, // Accepts 'true' and 'false'. If false, then attribute2 on the right.
