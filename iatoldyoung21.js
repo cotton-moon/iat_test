047bf3f69b8c01511c2aa1ebfc92bef19f58cca1
cotton-moon <wooyueting@gmail.com>
2025-07-16 17:28:26 +0100

    Update and rename iatoldyoung20.js to iatoldyoung21.js

--- iatoldyoung21.js
+++ iatoldyoung21.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cotton-moon.github.io/iat_test/5blockiat5.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cotton-moon.github.io/iat_test/5blockiat6.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
