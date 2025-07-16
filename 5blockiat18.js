6574d1c36d37da10f15fe16cfc54b5d58db9ef5d
cotton-moon <wooyueting@gmail.com>
2025-07-16 20:36:13 +0100

    Update and rename 5blockiat17.js to 5blockiat18.js

--- 5blockiat18.js
+++ 5blockiat18.js
@@ -121,7 +121,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			blockCategories_nTrials : 12,
 			blockCategories_nMiniBlocks : 12,
 			blockFirstCombined_nTrials : 24,
-			blockFirstCombined_nMiniBlocks : 24,
+			blockFirstCombined_nMiniBlocks : 12,
 			blockSecondCombined_nTrials : 0, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
 			blockSecondCombined_nMiniBlocks : 10, 
 			blockSwitch_nTrials : 12,
