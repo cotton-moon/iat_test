4ed191f268ef4fc23d27dde7a2dc89c7f7ec3212
cotton-moon <wooyueting@gmail.com>
2025-07-16 13:54:23 +0100

    Update and rename iatoldyoung2.js to iatoldyoung1.js

--- iatoldyoung1.js
+++ iatoldyoung1.js
@@ -11,11 +11,11 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
     		    {image : 'black1.jpg'}, 
-    			{image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}
+    			{image : 'black2.jpg'}, 
+    			{image : 'black3.jpg'}, 
+    			{image : 'black4.jpg'}, 
+    			{image : 'black5.jpg'}, 
+    			{image : 'black6.jpg'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
@@ -28,18 +28,18 @@ define(['pipAPI', 'https://baranan.github.io/minno-tasks/quiat8.js'], function(A
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}, 
-    			{image : 'old1.jpg'}			], 
+    		    {image : 'yf1.jpg'}, 
+    			{image : 'yf4.jpg'}, 
+    			{image : 'yf5.jpg'}, 
+    			{image : 'ym2.jpg'}, 
+    			{image : 'ym3.jpg'}, 
+    			{image : 'ym5.jpg'}			], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://cotton-moon.github.io/iat_test'
+			image : 'https://baranan.github.io/minno-tasks/images/'
 		} 
 	});
 });
