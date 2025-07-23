3554b6ec9439a1ed4a4aac940d7ce24aff97f032
cotton-moon <wooyueting@gmail.com>
2025-07-23 12:06:46 +0100

    Update and rename 5blockIATrandom to 5blockIATrandom.js

--- 5blockIATrandom.js
+++ 5blockIATrandom.js
@@ -30,86 +30,88 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			},
 			//When scoring, we will consider the compatible condition the pairing condition that requires response with one key to [category1,attribute1] and the other key to [category2,attribute2]
 			category1 : {
-				name : 'Black people', //Will appear in the data and in the default feedback message.
+				name : 'Old', //Will appear in the data and in the default feedback message.
 				title : {
-					media : {word : 'Black people'}, //Name of the category presented in the task.
+					media : {word : 'Old'}, //Name of the category presented in the task.
 					css : {color:'#336600','font-size':'1.8em'}, //Style of the category title.
 					height : 4 //Used to position the "Or" in the combined block.
 				},
 				stimulusMedia : [ //Stimuli content as PIP's media objects
-					{word: 'Tyron'},
-					{word: 'Malik'},
-					{word: 'Terrell'},
-					{word: 'Jazmin'},
-					{word: 'Tiara'},
-					{word: 'Shanice'}
+					{image: 'old1.jpg'},
+					{image: 'old2.jpg'},
+					{image: 'old3.jpg'},
+					{image: 'old4.jpg'},
+					{image: 'old5.jpg'},
+					{image: 'old6.jpg'}
 				],
 				//Stimulus css (style)
-				stimulusCss : {color:'#336600','font-size':'2.3em'}
+				stimulusCss : {color:'#31940F','font-size':'1.8em',
+				      width: '200px',
+   	 				height: '250px',
+    					objectFit: 'cover'}
 			},
 			category2 :	{
-				name : 'White people', //Will appear in the data and in the default feedback message.
+				name : 'Young', //Will appear in the data and in the default feedback message.
 				title : {
-					media : {word : 'White people'}, //Name of the category presented in the task.
+					media : {word : 'Young'}, //Name of the category presented in the task.
 					css : {color:'#336600','font-size':'1.8em'}, //Style of the category title.
 					height : 4 //Used to position the "Or" in the combined block.
 				},
 				stimulusMedia : [ //Stimuli content as PIP's media objects
-					{word: 'Jake'},
-					{word: 'Connor'},
-					{word: 'Bradley'},
-					{word: 'Allison'},
-					{word: 'Emma'},
-					{word: 'Emily'}
+					{image: 'young1.jpg'},
+					{image: 'young2.jpg'},
+					{image: 'young3.jpg'},
+					{image: 'young4.jpg'},
+					{image: 'young5.jpg'},
+					{image: 'young6.jpg'}
 				],
 				//Stimulus css
-				stimulusCss : {color:'#336600','font-size':'2.3em'}
+				stimulusCss : {color:'#31940F','font-size':'1.8em',
+				      width: '200px',
+   	 				height: '250px',
+    					objectFit: 'cover'}
 			},
 			attribute1 :
 			{
-				name : 'Bad words',
+				name : 'Inactive',
 				title : {
-					media : {word : 'Bad words'},
+					media : {word : 'Inactive'},
 					css : {color:'#0000FF','font-size':'1.8em'},
 					height : 4 //Used to position the "Or" in the combined block.
 				},
 				stimulusMedia : [ //Stimuli content as PIP's media objects
-					{word: 'awful'},
-					{word: 'failure'},
-					{word: 'agony'},
-					{word: 'hurt'},
-					{word: 'horrible'},
-					{word: 'terrible'},
-					{word: 'nasty'},
-					{word: 'evil'}
+					{word: 'forgetful'},
+					{word: 'lethargic'},
+					{word: 'slow'},
+					{word: 'tired'},
+					{word: 'weak'},
+					{word: 'weary'}
 				],
 				//Stimulus css
 				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 			},
 			attribute2 :
 			{
-				name : 'Good words',
+				name : 'Active',
 				title : {
-					media : {word : 'Good words'},
+					media : {word : 'Active'},
 					css : {color:'#0000FF','font-size':'1.8em'},
 					height : 4 //Used to position the "Or" in the combined block.
 				},
 				stimulusMedia : [ //Stimuli content as PIP's media objects
-					{word: 'laughter'},
-					{word: 'happy'},
-					{word: 'glorious'},
-					{word: 'joy'},
-					{word: 'wonderful'},
-					{word: 'peace'},
-					{word: 'pleasure'},
-					{word: 'love'}
+					{word: 'dynamic'},
+					{word: 'energetic'},
+					{word: 'fast'},
+					{word: 'lively'},
+					{word: 'quick'},
+					{word: 'strong'}
 				],
 				//Stimulus css
 				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 			},
 
 			base_url : {//Where are your images at?
-				image : '/implicit/user/yba/pipexample/biat/images/'
+				image : 'https://cotton-moon.github.io/iat_test'
 			},
 
 			//nBlocks : 7, This is not-supported anymore. If you want a 5-block IAT, change blockSecondCombined_nTrials to 0.
@@ -117,15 +119,15 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			////In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
 			////If you set the number of trials in any block to 0, that block will be skipped.
 			blockAttributes_nTrials : 12,
-			blockAttributes_nMiniBlocks : 2,
+			blockAttributes_nMiniBlocks : 1,
 			blockCategories_nTrials : 12,
-			blockCategories_nMiniBlocks : 2,
+			blockCategories_nMiniBlocks : 1,
 			blockFirstCombined_nTrials : 24,
-			blockFirstCombined_nMiniBlocks : 4,
+			blockFirstCombined_nMiniBlocks : 1,
 			blockSecondCombined_nTrials : 0, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
 			blockSecondCombined_nMiniBlocks : 10, 
 			blockSwitch_nTrials : 12,
-			blockSwitch_nMiniBlocks : 2,
+			blockSwitch_nMiniBlocks : 1,
 
 			//Should we randomize which attribute is on the right, and which on the left?
 			randomAttSide : false, // Accepts 'true' and 'false'. If false, then attribute2 on the right.
